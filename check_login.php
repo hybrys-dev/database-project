c13e5b17661203c73d6103dc546c02fe8d8d6c9f
hybrys-dev <73282816+hybrys-dev@users.noreply.github.com>
2024-04-12 00:06:13 +0200

    Add files via upload
    new fix

---
+++ check_login.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+
+    $valid_username = "admin";
+    $valid_password = "admin";
+
+    if($POST["username"] === $valid_username && $POST["password"] === $valid_password)
+    {
+        $_SESSION["logged_in"] = true;
+        $_SESSION["username"] = $username;
+        header("Location: main.php");
+        exit();
+    }
+    else
+    {
+        header("Location:index.php?error=1");
+        exit();
+    }
+?>
--- main.php
+++ main.php
@@ -3,7 +3,7 @@
     if(!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true)
     {
         header("location: login.php");
-        exit;
+        exit();
     }
 ?>
 
